82cdae8a025fc5032e4f3456713b228ae88ddf10
Maxwell-SS <shingletonsmith@gmail.com>
2021-09-22 21:49:05 -0400

    Init

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+// openGL
+#include <glad/glad.h>
+
+// glfw
+#include <GLFW/glfw3.h>
+
+// glm
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/rotate_vector.hpp>
+#include <glm/gtx/vector_angle.hpp>
+
+// imgui
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+// json
+// pass
+
+// stb
+#include <stb_image.h>
+
+// std
+#include <iostream>
+
+int main() {
+	std::cout << "All files included successfully." << std::endl;
+	return 0;
+}
